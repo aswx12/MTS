ed6991446f43e11e78e350512d0695940a11f9da
Hoang <aswx12@gmail.com>
2023-02-02 16:38:34 +0100

    mores tuff

---
+++ theDefault/src/main/java/suikaMod/cards/CustomCards/fasffssf.java
@@ -0,0 +1,74 @@
+package suikaMod.cards.CustomCards;
+
+import basemod.AutoAdd;
+import basemod.BaseMod;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.localization.LocalizedStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import suikaMod.DefaultMod;
+import suikaMod.cards.AbstractDynamicCard;
+import suikaMod.characters.TheDefault;
+
+import static suikaMod.DefaultMod.makeCardPath;
+
+import java.io.File;  // Import the File class
+import java.io.IOException;
+
+import static com.megacrit.cardcrawl.core.CardCrawlGame.languagePack;
+
+
+@AutoAdd.Seen
+public class fasffssf extends AbstractDynamicCard
+{
+    public static final String ID = DefaultMod.makeID(fasffssf.class.getSimpleName()); 
+    public static final String IMG = makeCardPath("Attack.png"); 
+    private static final CardRarity RARITY = CardRarity.COMMON; //  Up to you, I like auto-complete on these
+    private static final CardTarget TARGET = CardTarget.ENEMY;  //   since they don't change much.
+    private static final CardType TYPE = CardType.ATTACK;       //
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 12;  
+    private static final int UPGRADED_COST = 12; 
+
+    public static int DAMAGE = 1; 
+    private static final int UPGRADE_PLUS_DMG = 12;  
+
+    // /STAT DECLARATION/
+
+    public fasffssf ()
+    { 
+        super(ID, "fasffssf", IMG,"Deal !D! damage", COST, TYPE, COLOR, RARITY, TARGET);
+        baseDamage = DAMAGE;
+        this.tags.add(CardTags.STARTER_STRIKE); 
+        this.tags.add(CardTags.STRIKE);
+
+    }
+
+
+    // Actions the card should do.
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m)
+    {
+        AbstractDungeon.actionManager.addToBottom(
+                new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+    }
+
+    // Upgraded stats.
+    @Override
+    public void upgrade()
+    {
+        if (!upgraded)
+        {
+            upgradeName();
+            upgradeDamage(UPGRADE_PLUS_DMG);
+            upgradeBaseCost(UPGRADED_COST);
+            initializeDescription();
+        }
+    }
+}
